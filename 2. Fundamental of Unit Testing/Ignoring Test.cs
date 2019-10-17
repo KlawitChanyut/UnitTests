128e8f6b0ae6cf570c69272514ef4388764f7c88
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 04:15:18 +0700

    Update Test , TestCase

--- 2. Fundamental of Unit Testing/Ignoring Test.cs
+++ 2. Fundamental of Unit Testing/Ignoring Test.cs
@@ -12,5 +12,23 @@ namespace TestNinjaUnit._2.Fundamental_of_Unit_Testing
         {
             _math = new Math();
         }
+        [Test]
+        [Ignore("Because I wanted to!")]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
     }
 }
