a8d32c7957ea0dcdf48b0f7367fcb9f56c0d22da
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 04:54:36 +0700

    Create TestFixture Testing_Array_and_Collection

---
+++ 3. Core Unit Testing Techniques/Testing Array and Collection.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._3.Core_Unit_Testing_Techingues
+{
+    [TestFixture]
+    class Testing_Array_and_Collection
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        //        [Ignore("Because I wanted to!")]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
