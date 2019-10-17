705b1ca639f7d07d3febe3c23053b8fb632c8c02
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 05:06:18 +0700

    Create TestFixture Testing_the_Return_Type_of_Method

---
+++ 3. Core Unit Testing Techniques/Testing the Return Type of Method.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._3.Core_Unit_Testing_Techingues
+{
+    [TestFixture]
+    class Testing_the_Return_Type_of_Method
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            // NotFound 
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //NotFound or one of its derivatives 
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+        }
+    }
+}
