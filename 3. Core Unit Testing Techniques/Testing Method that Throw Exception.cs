a55b4b1e5057f2503840a8c6319d4e657d23e39e
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 05:26:24 +0700

    Create TestFixture Testing_Method_That_throw_Exception

---
+++ 3. Core Unit Testing Techniques/Testing Method that Throw Exception.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._3.Core_Unit_Testing_Techingues
+{
+    [TestFixture]
+    class Testing_Method_That_throw_Exception
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
