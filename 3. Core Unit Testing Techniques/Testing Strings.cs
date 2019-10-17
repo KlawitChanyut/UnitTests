78ff8f26d62bdff28787b81843eac6a0014ab7a6
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 04:50:38 +0700

    Create TestFixure Testing_Strings

---
+++ 3. Core Unit Testing Techniques/Testing Strings.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._3.Core_Unit_Testing_Techingues
+{
+    [TestFixture]
+    class Testing_Strings
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+           
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
