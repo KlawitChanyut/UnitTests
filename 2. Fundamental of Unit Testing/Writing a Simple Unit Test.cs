02993a4a439588cc6b4415782599ab53a4d7ac89
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 03:38:57 +0700

    Create TestFixture Writing_a_Simple_Unit_Test

---
+++ 2. Fundamental of Unit Testing/Writing a Simple Unit Test.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._2.Fundamental_of_Unit_Testing
+{
+    [TestFixture]
+    class Writing_a_Simple_Unit_Test
+    {
+    }
+}
