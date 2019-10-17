b4ee07d6344135fefef94e044039d9eaedce77d3
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 04:07:32 +0700

    Update TestCase

--- 2. Fundamental of Unit Testing/Parameterized Test.cs
+++ 2. Fundamental of Unit Testing/Parameterized Test.cs
@@ -19,5 +19,15 @@ namespace TestNinjaUnit._2.Fundamental_of_Unit_Testing
 
             Assert.That(result, Is.EqualTo(3));
         }
+        [Test]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
     }
 }
