7f07e78854da0efa15da1b769aeb18b5dac84749
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 03:01:02 +0700

    Create Using NUnit in Visual Studio.cs

---
+++ 1. Getting Started/Using NUnit in Visual Studio.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._1.Getting_Started
+{
+    [TestFixture]
+    class Using_NUnit_in_Visual_Studio
+    {
+        [Test]
+        public void CanCalledBy_UserIsAdmin_ReturnTrue()
+        {
+            var reservation = new Reservation();
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
