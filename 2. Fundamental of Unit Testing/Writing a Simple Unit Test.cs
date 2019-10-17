d43b4aba866b89ab63991e2834b05751835c2642
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 03:44:01 +0700

    Update Test Add_WhenCalled_ReturnTheSumOfArguments

--- 2. Fundamental of Unit Testing/Writing a Simple Unit Test.cs
+++ 2. Fundamental of Unit Testing/Writing a Simple Unit Test.cs
@@ -6,5 +6,14 @@ namespace TestNinjaUnit._2.Fundamental_of_Unit_Testing
     [TestFixture]
     class Writing_a_Simple_Unit_Test
     {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
     }
 }
