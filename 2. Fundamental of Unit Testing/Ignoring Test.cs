cd19f2ed74851325f6e2bbdc109477598fe796da
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 04:11:37 +0700

    Create TestFixture Ignoring_Test

---
+++ 2. Fundamental of Unit Testing/Ignoring Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._2.Fundamental_of_Unit_Testing
+{
+    [TestFixture]
+    class Ignoring_Test
+    {
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+    }
+}
