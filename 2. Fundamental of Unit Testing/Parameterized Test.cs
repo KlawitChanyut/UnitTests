be33c209e079a0a23a0e45daacc9b3f1ddc4a10d
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 04:05:02 +0700

    Create TestFixture Parameterized_Test

---
+++ 2. Fundamental of Unit Testing/Parameterized Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._2.Fundamental_of_Unit_Testing
+{
+    [TestFixture]
+    class Parameterized_Test
+    {
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
