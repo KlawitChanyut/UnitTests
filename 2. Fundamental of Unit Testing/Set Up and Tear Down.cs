6e9b415cd5bdfd11ad38654116b60555aafdeb32
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 03:56:25 +0700

    Create TestFixture Set_Up_and_Tear_Down

---
+++ 2. Fundamental of Unit Testing/Set Up and Tear Down.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._2.Fundamental_of_Unit_Testing
+{
+    [TestFixture]
+    class Set_Up_and_Tear_Down
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {         
+            var result = _math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
