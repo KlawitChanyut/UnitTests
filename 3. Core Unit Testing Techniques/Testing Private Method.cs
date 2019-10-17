7225291a1ab34e81a2e8e5f4f33e767aefac7ff1
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 05:39:20 +0700

    Create TestFixture Testing_Private_Method

---
+++ 3. Core Unit Testing Techniques/Testing Private Method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._3.Core_Unit_Testing_Techingues
+{
+    [TestFixture]
+    class Testing_Private_Method
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
