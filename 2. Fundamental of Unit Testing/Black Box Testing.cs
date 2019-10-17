f3fae529ae36e87dae33486db4f0f91506b8013a
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 03:46:21 +0700

    Create Black Box Testing.cs

---
+++ 2. Fundamental of Unit Testing/Black Box Testing.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._2.Fundamental_of_Unit_Testing
+{
+    [TestFixture]
+    class Black_Box_Testing
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
