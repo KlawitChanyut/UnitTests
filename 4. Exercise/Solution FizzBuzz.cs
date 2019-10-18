c04c1d68a557e3de8677561b6c0f1f8e57124bd5
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 13:18:21 +0700

    Create TestFixture Solution_FizzBuzz

---
+++ 4. Exercise/Solution FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+
+namespace TestNinjaUnit._4.Exercise
+{
+    [TestFixture]
+    public class Solution_FizzBuzz
+    {
+        private Exercise_Fizz_Buzz FizzBuzz;
+        [SetUp]
+        public void SetUp()
+        {
+            FizzBuzz = new Exercise_Fizz_Buzz();
+        }
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+        {
+            var result = Exercise_Fizz_Buzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+    }
+}
