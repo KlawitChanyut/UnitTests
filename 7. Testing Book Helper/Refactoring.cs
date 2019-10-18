96251463e968e1aba9f37540b95a288a32a62c64
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 03:42:19 +0700

    Create TestFixture Refactoring

---
+++ 7. Testing Book Helper/Refactoring.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinjaUnit._7.Testing_Book_Helper
+{
+    [TestFixture]
+    public class Refactoring
+    {
+        private Booking _existingBooking;
+        private Mock<IBookingRepository> _repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _existingBooking = new Booking
+            {
+                Id = 2,
+                ArrivalDate = ArriveOn(2017, 1, 15),
+                DepartureDate = DepartOn(2017, 1, 20),
+                Reference = "a"
+
+            };
+
+            _repository = new Mock<IBookingRepository>();
+
+            _repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
+            {
+               _existingBooking
+
+            }.AsQueryable()); ;
+        }
+    }
+}
