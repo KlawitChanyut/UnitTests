c80661789065f3e521d1576afb7a6b0565217f6a
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 03:15:21 +0700

    Create TestFixture Testing_Employee_Controller

---
+++ 6. Exercise/Testing Employee Controller.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinjaUnit._6.Exercise
+{
+    [TestFixture]
+    class Testing_Employee_Controller
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+        {
+            var storage = new Mock<IEmployee_Storage>();
+            var controller = new Employee_Controller(storage.Object);
+
+            controller.DeleteEmployee(1);
+
+            storage.Verify(s => s.DeleteEmployee(1));
+        }
+    }
+}
