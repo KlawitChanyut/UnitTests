14ef610a501c3d95b644307d04a80757a18338b9
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 04:19:02 +0700

    Update Test BookingStartAndFinishesInTheMiddleOfAnExistingBooking_ReturnExistingBookingReference

--- 7. Testing Book Helper/Writing Additional Tests.cs
+++ 7. Testing Book Helper/Writing Additional Tests.cs
@@ -73,5 +73,17 @@ namespace TestNinjaUnit._7.Testing_Book_Helper
 
             Assert.That(result, Is.EqualTo(_existingBooking.Reference));
         }
+        [Test]
+        public void BookingStartAndFinishesInTheMiddleOfAnExistingBooking_ReturnExistingBookingReference()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(new Booking
+            {
+                Id = 1,
+                ArrivalDate = After(_existingBooking.ArrivalDate),
+                DepartureDate = Before(_existingBooking.DepartureDate)
+            }, _repository.Object);
+
+            Assert.That(result, Is.EqualTo(_existingBooking.Reference));
+        }
     }
 }
