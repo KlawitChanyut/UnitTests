8e9f67b5afd5e6e90fd1518d019717bd33f711aa
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 02:10:30 +0700

    Create TestFixture Testing_Video_Service

---
+++ 6. Exercise/Testing Video Service.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Moq;
+using TestNinja.Mocking;
+using System.Collections.Generic;
+
+namespace TestNinjaUnit._6.Exercise
+{
+    [TestFixture]
+    class Testing_Video_Service
+    {
+        private VideoServiceD _videoService;
+        private Mock<IFileReader> _fileReader;
+        private Mock<IRefacVideoRepository> _repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _repository = new Mock<IRefacVideoRepository>();
+            _videoService = new VideoServiceD(_fileReader.Object,_repository.Object);
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            var result = _videoService.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
