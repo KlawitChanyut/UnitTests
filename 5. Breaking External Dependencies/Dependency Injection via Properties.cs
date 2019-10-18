2cebc3821ff74b8bc803176adb7d47391e200157
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 01:26:13 +0700

    Create TestFixture Dependency_Injection_via_Properties

---
+++ 5. Breaking External Dependencies/Dependency Injection via Properties.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinjaUnit._5.Breaking_External_Dependencies
+{
+    [TestFixture]
+    class Dependency_Injection_via_Properties
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new VideoServiceA();
+
+            service.fileReader = new FakeFileReader();
+
+            var result = service.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
