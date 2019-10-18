de80c9d491bf8ff4b5646427e90ae43b40316d55
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 01:19:18 +0700

    Create class Refactoring_Towards_a_Loosely_copled_Design

---
+++ 5. Breaking External Dependencies/Refactoring Towards a Loosely copled Design.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System;
+
+namespace TestNinjaUnit._5.Breaking_External_Dependencies
+{
+    class Refactoring_Towards_a_Loosely_copled_Design
+    {
+        public interface IFileReader
+        {
+            string Read(string path);
+        }
+
+        public class FileReader : IFileReader
+        {
+            public string Read(string path)
+            {
+                return File.ReadAllText(path);
+            }
+        }
+    }
+}
