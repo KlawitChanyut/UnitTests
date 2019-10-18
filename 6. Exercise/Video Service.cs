69a612dcfec4102dfc26cd8503609b7c8bcb8e9e
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 01:45:53 +0700

    Create class Video_Service

---
+++ 6. Exercise/Video Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TestNinja.Mocking
+{
+    public class Video_Service
+    {
+        public string ReadVideoTitle(IFileReader fileReader)
+        {
+            var str = fileReader.Read("video.txt");
+            var video = JsonConvert.DeserializeObject<Video>(str);
+            if (video == null)
+                return "Error parsing the video.";
+            return video.Title;
+        }
+
+        public string GetUnprocessedVideosAsCsv()
+        {
+            var videoIds = new List<int>();
+
+            using (var context = new VideoContext())
+            {
+                var videos =
+                    (from video in context.Videos
+                     where !video.IsProcessed
+                     select video).ToList();
+
+                foreach (var v in videos)
+                    videoIds.Add(v.Id);
+
+                return String.Join(",", videoIds);
+            }
+        }
+    }
+}
