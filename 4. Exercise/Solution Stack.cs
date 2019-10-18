91d268594f93bc5a664eb467bca556bda2044238
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 13:37:09 +0700

    Create TestFixture Solution_Stack

---
+++ 4. Exercise/Solution Stack.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._4.Exercise
+{
+    [TestFixture]
+    class Solution_Stack
+    {
+        [Test]
+        public void Push_ArgIsNull_ThrowArgNullException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+    }
+}
