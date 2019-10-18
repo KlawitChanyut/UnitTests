9909a4a92cba55f9c724e7e00b0d759060e55c6c
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 03:19:14 +0700

    Create class Refactoring_Employee_Controller

---
+++ 6. Exercise/Refactoring Employee Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public class Refactoring_Employee_Controller
+    {
+        private readonly IEmployee_Storage _storage;
+
+        public Refactoring_Employee_Controller(IEmployee_Storage storage)
+        {
+            _storage = storage;
+        }
+
+        public ActionResult DeleteEmployee(int id)
+        {
+            _storage.DeleteEmployee(id);
+
+            return RedirectToAction("Employees");
+        }
+
+        private ActionResult RedirectToAction(string employees)
+        {
+            return new RedirectResult();
+        }
+    }
+}
