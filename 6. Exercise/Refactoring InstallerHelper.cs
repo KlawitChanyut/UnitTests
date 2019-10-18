40649d91bd9309311c11453bf1916b5c98b55a7b
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 03:08:56 +0700

    Create interface IRefactoring_InstallerHelper

---
+++ 6. Exercise/Refactoring InstallerHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public interface IRefactoring_InstallerHelper
+    {
+        void DownloadFile(string url, string path);
+    }
+
+    public class RefacInstallerHelper : IRefactoring_InstallerHelper
+    {
+        public void DownloadFile(string url, string path)
+        {
+            var client = new WebClient();
+            client.DownloadFile(url, path);
+        }
+    }
+}
