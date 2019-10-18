583eecdaa036e54e27b75ed665293d26872d4e51
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 05:10:03 +0700

    Create TestFixture Testing_Exceptions

---
+++ 8. TestingHouseKeeper/Testing Exceptions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinjaUnit._8.TestingHouseKeeper
+{
+    [TestFixture]
+    class Testing_Exceptions
+    {
+        private RefacHouseKeeperHelper _service;
+        private Mock<IStatementGenerator> _statementGenerator;
+        private Mock<IEmailSender> _emailSender;
+        private Mock<IXtraMessageBox> _messageBox;
+        private DateTime _statementDate = new DateTime(2017, 1, 1);
+        private Housekeeper _houseKeeper;
+        private string _statementFileName;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _houseKeeper = new Housekeeper { Email = "a", FullName = "b", Oid = 1, StatementEmailBody = "c" };
+
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(uow => uow.Query<Housekeeper>()).Returns(new List<Housekeeper>
+            {
+                _houseKeeper
+            }.AsQueryable());
+
+            _statementFileName = "fileName";
+            _statementGenerator = new Mock<IStatementGenerator>();
+            _statementGenerator
+                .Setup(sg => sg.SaveStatement(_houseKeeper.Oid, _houseKeeper.FullName, (_statementDate)))
+                .Returns(() => _statementFileName);
+
+            _emailSender = new Mock<IEmailSender>();
+            _messageBox = new Mock<IXtraMessageBox>();
+
+            _service = new RefacHouseKeeperHelper(
+                unitOfWork.Object,
+                _statementGenerator.Object,
+                _emailSender.Object,
+                _messageBox.Object);
+        }
+
+        [Test]
+        public void SendStatementEmails_WhenCalled_GenerateStatements()
+        {
+            _service.SendStatementEmails(_statementDate);
+
+            _statementGenerator.Verify(sg =>
+                sg.SaveStatement(_houseKeeper.Oid, _houseKeeper.FullName, (_statementDate)));
+        }
+    }
+}
