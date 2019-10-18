6ddddcbc64d8ce22d44333426993807ccc288970
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 13:28:54 +0700

    Update Test , TestCase CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints

--- 4. Exercise/Solution Demerit Point Calculator.cs
+++ 4. Exercise/Solution Demerit Point Calculator.cs
@@ -17,5 +17,21 @@ namespace TestNinjaUnit._4.Exercise
             Assert.That(() => calculator.CalculateDemeritPoints(speed),
                 Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
         }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
+        {
+            var calculator = new DemeritPointsCalculator();
+
+            var points = calculator.CalculateDemeritPoints(speed);
+
+            Assert.That(points, Is.EqualTo(expectedResult));
+        }
     }
 }
