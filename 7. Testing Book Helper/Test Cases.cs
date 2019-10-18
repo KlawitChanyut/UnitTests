de8052c78e84911dde3d0cab824dfaa96427c852
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 03:35:37 +0700

    Create TestFixture Test_Cases

---
+++ 7. Testing Book Helper/Test Cases.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+using Moq;
+
+namespace TestNinjaUnit._7.Testing_Book_Helper
+{
+    [TestFixture]
+    public class Test_Cases
+    {
+        [Test]
+        public void OverlappingBookingsExist_BookingStartAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+        {
+
+        }
+    }
+}
