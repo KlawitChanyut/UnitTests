061a689c827e6f3276986a16ad40ecd72a00f5bd
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 01:22:33 +0700

    Create TestFixture Dependency_Injection_via_Method_Parameters

---
+++ 5. Breaking External Dependencies/Dependency Injection via Method Parameters.cs
@@ -0,0 +1,19 @@
+﻿using TestNinja.Mocking;
+using NUnit.Framework;
+
+
+namespace TestNinjaUnit._5.Breaking_External_Dependencies
+{
+    [TestFixture]
+    class Dependency_Injection_via_Method_Parameters
+    {    
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            var service = new Video_Service();
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
