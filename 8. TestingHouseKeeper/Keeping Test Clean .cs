a3af4e2a035b2df92f960e625ab0f702ba813fa8
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 04:57:12 +0700

    Create TestFixture Keeping_Test_Clean

---
+++ 8. TestingHouseKeeper/Keeping Test Clean .cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinjaUnit._8.TestingHouseKeeper
+{
+    [TestFixture]
+    public class Keeping_Test_Clean
+    {
+        private RefacHouseKeeperHelper _service;
+        private Mock<IStatementGenerator> _statementGenerator;
+        private Mock<IEmailSender> _emailSender;
+        private Mock<IXtraMessageBox> _messageBox;
+        private DateTime _statementDate = new DateTime(2017, 1, 1);
+        private Housekeeper _houseKeeper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var unitOfWork = new Mock<IUnitOfWork>();
+            _houseKeeper = new Housekeeper
+            {
+                Email = "a",
+                FullName = "b",
+                Oid = 1,
+                StatementEmailBody = "c"
+            };
+            unitOfWork.Setup(uow => uow.Query<Housekeeper>()).Returns(new List<Housekeeper>
+            {
+                _houseKeeper
+            }.AsQueryable());
+
+            _statementGenerator = new Mock<IStatementGenerator>();
+            _emailSender = new Mock<IEmailSender>();
+            _messageBox = new Mock<IXtraMessageBox>();
+
+            _service = new RefacHouseKeeperHelper(unitOfWork.Object
+                , _statementGenerator.Object,
+                _emailSender.Object,
+                _messageBox.Object);
+        }
+
+        [Test]
+        public void SendStatementEmails_WhenCalled_GenerateStatement()
+        {
+            _service.SendStatementEmails(_statementDate);
+
+            _statementGenerator.Verify(sg => sg.SaveStatement(_houseKeeper.Oid,
+                _houseKeeper.FullName,
+                (_statementDate)));
+        }
+    }
+}
