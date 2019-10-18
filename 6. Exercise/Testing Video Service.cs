979b2ec6c19106e60e799a09d2051e9a3de7ebff
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 03:03:21 +0700

    Update Test GetUnprocessedVideosAsCsv_AFewUnProcessed_ReturnAStringWithIdOfUnprocessedVideos

--- 6. Exercise/Testing Video Service.cs
+++ 6. Exercise/Testing Video Service.cs
@@ -39,5 +39,21 @@ namespace TestNinjaUnit._6.Exercise
 
             Assert.That(result, Is.EqualTo(""));
         }
+
+        [Test]
+        public void GetUnprocessedVideosAsCsv_AFewUnProcessed_ReturnAStringWithIdOfUnprocessedVideos()
+        {
+            _repository.Setup(r => r.GetUnprocessedVideos()).Returns(new List<Video>
+            {
+                new Video {Id = 1},
+                new Video {Id = 2},
+                new Video {Id = 3},
+
+            });
+
+            var result = _videoService.GetUnprocessedVideosAsCsv();
+
+            Assert.That(result, Is.EqualTo("1,2,3"));
+        }
     }
 }
