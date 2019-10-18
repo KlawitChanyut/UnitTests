6ca9be119c1e0b8c8c71f851fdc3c3d40eb55ef9
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-19 01:38:20 +0700

    Create TestFixture Testing_the_Interaction_Between_Two_Objects

---
+++ 5. Breaking External Dependencies/Testing the Interaction Between Two Objects.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Moq;
+using TestNinja.Mocking;
+
+namespace TestNinjaUnit._5.Breaking_External_Dependencies
+{
+    [TestFixture]
+    class Testing_the_Interaction_Between_Two_Objects
+    {
+        [Test]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            var storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
