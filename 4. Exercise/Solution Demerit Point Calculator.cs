a95e8ea8f82d06e5d6a22bae48caa0c933255430
KlawitChanyut <56555868+KlawitChanyut@users.noreply.github.com>
2019-10-18 13:25:56 +0700

    Create TestFixture Solution_Demerit_Point_Calculator

---
+++ 4. Exercise/Solution Demerit Point Calculator.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaUnit._4.Exercise
+{
+    [TestFixture]
+    class Solution_Demerit_Point_Calculator
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeException(int speed)
+        {
+            var calculator = new DemeritPointsCalculator();
+
+            Assert.That(() => calculator.CalculateDemeritPoints(speed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
